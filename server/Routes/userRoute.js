539b2b2f89ab59f75a6a7898059d444e7dce3038
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-08 00:39:00 +0300

    Fix minor bug

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userController = require("../Controllers/userController");
 const router = express.Router();
 
-router.use("/register", userController.register);
-router.use("/login", userController.login);
+router.post("/register", userController.register);
+router.post("/login", userController.login);
 
 module.exports = router;
