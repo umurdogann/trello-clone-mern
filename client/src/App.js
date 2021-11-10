03d8f77d2ff5b57439b752b6e1ee74eef5969361
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-10 05:05:50 +0300

    Create App.js using with react-router-dom to navigate between pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
+import Index from "./Components/Pages/Index";
+import Login from "./Components/Pages/Login";
+import Register from "./Components/Pages/Register";
+import Alert from "./Components/AlertSnackBar";
+import { BrowserRouter, Switch } from "react-router-dom";
+import Boards from "./Components/Pages/Boards";
+import ProtectedRoute from "./Utils/ProtectedRoute";
+import { loadUser } from "./Services/userService";
+import Store from "./Redux/Store";
+import FreeRoute from "./Utils/FreeRoute";
 const App = () => {
+  useEffect(() => {
+    loadUser(Store.dispatch);
+  }, []);
   return (
-    <div>
-      This is trello clone!
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Alert />
+      <Switch>
+        <ProtectedRoute exact path="/boards" component={Boards} />
+        <FreeRoute exact path="/login" component={Login} />
+        <FreeRoute exact path="/register" component={Register} />
+        <FreeRoute exact path="/" component={Index} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
