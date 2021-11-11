8581c80391a12e1ccb52edfea332b5821f3ba078
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-11 14:48:45 +0300

    Change loading screen view

--- client/src/Components/LoadingScreen.js
+++ client/src/Components/LoadingScreen.js
@@ -1,11 +1,24 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import LinearProgress from "@mui/material/LinearProgress";
+import Backdrop from "@mui/material/Backdrop";
+import styled from "styled-components";
+const Icon = styled.img`
+  width: 10vw;
+`;
 
 export default function LoadingScreen() {
+  const [open, setOpen] = React.useState(true);
+ /*  const handleClose = () => {
+    setOpen(false);
+  }; */
   return (
-    <Box sx={{ width: "100%" }}>
-      <LinearProgress />
-    </Box>
+    <div>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        //onClick={handleClose}
+      >
+        <Icon src="https://a.trellocdn.com/prgb/dist/images/header-logo-spirit-loading.87e1af770a49ce8e84e3.gif" />
+      </Backdrop>
+    </div>
   );
 }
