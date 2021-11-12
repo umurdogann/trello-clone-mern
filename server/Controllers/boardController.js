31689986ef7dfcaf77dbd071fc8385d546f5a80f
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-12 17:06:54 +0300

    Fix minor bug

--- server/Controllers/boardController.js
+++ server/Controllers/boardController.js
@@ -1,11 +1,11 @@
 const boardService = require("../Services/boardService");
 
 const create = async (req, res) => {
-  const { title, color } = req.body;
-  if (!(title && color))
+  const { title, backgroundImageLink } = req.body;
+  if (!(title && backgroundImageLink))
     return res
       .status(400)
-      .send({ errMessage: "Title and/or color cannot be null" });
+      .send({ errMessage: "Title and/or image cannot be null" });
   await boardService.create(req, (err, result) => {
     if (err) return res.status(500).send(err);
     result.__v = undefined;
