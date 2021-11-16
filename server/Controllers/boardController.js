c9e930d6fb1319a8316962a7829fc4311d677bba
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-17 01:35:54 +0300

    Add getById function to boardController

--- server/Controllers/boardController.js
+++ server/Controllers/boardController.js
@@ -1,27 +1,41 @@
-const boardService = require("../Services/boardService");
+const boardService = require('../Services/boardService');
 
 const create = async (req, res) => {
-  const { title, backgroundImageLink } = req.body;
-  if (!(title && backgroundImageLink))
-    return res
-      .status(400)
-      .send({ errMessage: "Title and/or image cannot be null" });
-  await boardService.create(req, (err, result) => {
-    if (err) return res.status(500).send(err);
-    result.__v = undefined;
-    return res.status(201).send(result);
-  });
+	const { title, backgroundImageLink } = req.body;
+	if (!(title && backgroundImageLink))
+		return res.status(400).send({ errMessage: 'Title and/or image cannot be null' });
+	await boardService.create(req, (err, result) => {
+		if (err) return res.status(500).send(err);
+		result.__v = undefined;
+		return res.status(201).send(result);
+	});
 };
 
 const getAll = async (req, res) => {
-  const userId = req.user.id;
-  await boardService.getAll(userId, (err, result) => {
-    if (err) return res.status(400).send(err);
-    return res.status(200).send(result);
-  });
+	const userId = req.user.id;
+	await boardService.getAll(userId, (err, result) => {
+		if (err) return res.status(400).send(err);
+		return res.status(200).send(result);
+	});
+};
+
+const getById = async (req, res) => {
+	// Validate whether params.id is in the user's boards or not
+	const validate = req.user.boards.filter((board) => board === req.params.id);
+	if (!validate)
+		return res
+			.status(400)
+			.send({ errMessage: 'You can not add a list to the board, you are not a member or owner!' });
+
+	// Call the service
+	await boardService.getById(req.params.id, (err, result) => {
+		if (err) return res.status(400).send(err);
+		return res.status(200).send(result);
+	});
 };
 
 module.exports = {
-  create,
-  getAll,
+	create,
+	getAll,
+	getById,
 };
