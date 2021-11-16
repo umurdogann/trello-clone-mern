44d447defdb1f934b3c2706e3796294026ed2074
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-17 01:38:54 +0300

    Create board service and add getLists, createList functions

---
+++ client/src/Services/boardService.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { setLoading, successCreatingList, successFetchingLists } from '../Redux/Slices/listSlice';
+import { openAlert } from '../Redux/Slices/alertSlice';
+
+const listRoute = 'http://localhost:3001/list';
+const boardRoute = 'httpallList://localhost:3001/board';
+
+export const getLists = async (boardId, dispatch) => {
+	dispatch(setLoading(true));
+	try {
+		const res = await axios.get(listRoute + '/' + boardId);
+		dispatch(successFetchingLists(res.data));
+		setTimeout(() => {
+			dispatch(setLoading(false));
+		}, 1000);
+	} catch (error) {
+		dispatch(setLoading(false));
+		dispatch(
+			openAlert({
+				message: error?.response?.data?.errMessage ? error.response.data.errMessage : error.message,
+				severity: 'error',
+			})
+		);
+	}
+};
+
+export const createList = async (title, boardId, dispatch) => {
+	dispatch(setLoading(true));
+	try {
+		const res = await axios.post(listRoute + '/create', {title:title, boardId:boardId});
+		dispatch(successCreatingList(res.data));
+		dispatch(setLoading(false));
+	} catch (error) {
+		dispatch(setLoading(false));
+		dispatch(
+			openAlert({
+				message: error?.response?.data?.errMessage ? error.response.data.errMessage : error.message,
+				severity: 'error',
+			})
+		);
+	}
+
+
+};
