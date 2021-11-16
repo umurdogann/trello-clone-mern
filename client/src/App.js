ae2fd75b9f39a5a36d96ba318a6a5768c0f1b7e9
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-17 01:44:51 +0300

    Add board route to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import ProtectedRoute from "./Utils/ProtectedRoute";
 import { loadUser } from "./Services/userService";
 import Store from "./Redux/Store";
 import FreeRoute from "./Utils/FreeRoute";
+import Board from "./Components/Pages/BoardPage/Board";
 const App = () => {
   useEffect(() => {
     loadUser(Store.dispatch);
@@ -18,6 +19,7 @@ const App = () => {
       <Alert />
       <Switch>
         <ProtectedRoute exact path="/boards" component={Boards} />
+        <ProtectedRoute exact path="/board/:id" component={Board} />
         <FreeRoute exact path="/login" component={Login} />
         <FreeRoute exact path="/register" component={Register} />
         <FreeRoute exact path="/" component={Index} />
