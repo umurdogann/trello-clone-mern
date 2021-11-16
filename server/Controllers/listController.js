da27512d9f7de62439a146f5b76fc18d61109af8
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-17 01:35:18 +0300

    Create listController and add create, getAll functions

---
+++ server/Controllers/listController.js
@@ -0,0 +1,43 @@
+const listService = require('../Services/listService');
+
+const create = async (req, res) => {
+	// Deconstruct the body
+	const { title, boardId } = req.body;
+	// Validate the title
+	if (!(title && boardId)) return res.status(400).send({ errMessage: 'Title cannot be empty' });
+
+	// Validate whether boardId is in the user's boards or not
+	const validate = req.user.boards.filter((board) => board === boardId);
+	if (!validate)
+		return res
+			.status(400)
+			.send({ errMessage: 'You can not add a list to the board, you are not a member or owner!' });
+
+	// Call the service to add new list
+	await listService.create({ title: title, owner: boardId }, req.user, (err, result) => {
+		if (err) return res.status(500).send(err);
+		return res.status(201).send(result);
+	});
+};
+
+const getAll = async (req, res) => {
+	// Assing parameter to constant
+	const boardId = req.params.id;
+
+	// Validate whether boardId is in the user's board or not
+	
+	const validate = req.user.boards.filter((board) => board === boardId);
+	if (!validate)
+		return res.status(400).send({ errMessage: 'You cannot get lists, because you are not owner of this lists!' });
+
+	// Call the service to get all lists whose owner id matches the boardId
+	await listService.getAll(boardId, (err, result) => {
+		if (err) return res.status(500).send(err);
+		return res.status(200).send(result);
+	});
+};
+
+module.exports = {
+	create,
+	getAll,
+};
