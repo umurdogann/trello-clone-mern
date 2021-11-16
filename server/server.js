1993614db13e27775ba5369e8e2587fc54ee8189
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-17 01:36:50 +0300

    Format server.js with new prettier and add list route

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-const dotenv = require("dotenv");
-const express = require("express");
-const unless = require("express-unless");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const userRoute = require("./Routes/userRoute");
-const boardRoute = require("./Routes/boardRoute");
-const auth = require("./Middlewares/auth");
+const dotenv = require('dotenv');
+const express = require('express');
+const unless = require('express-unless');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userRoute = require('./Routes/userRoute');
+const boardRoute = require('./Routes/boardRoute');
+const listRoute = require('./Routes/listRoute');
+const auth = require('./Middlewares/auth');
 
 dotenv.config();
 const app = express();
@@ -18,35 +19,36 @@ app.use(express.json());
 auth.verifyToken.unless = unless;
 
 app.use(
-  auth.verifyToken.unless({
-    path: [
-      { url: "/user/login", method: ["POST"] },
-      { url: "/user/register", method: ["POST"] },
-    ],
-  })
+	auth.verifyToken.unless({
+		path: [
+			{ url: '/user/login', method: ['POST'] },
+			{ url: '/user/register', method: ['POST'] },
+		],
+	})
 );
 
 //MONGODB CONNECTION
 
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database connection is succesfull!");
-  })
-  .catch((err) => {
-    console.log(`Database connection failed!`);
-    console.log(`Details : ${err}`);
-  });
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('Database connection is succesfull!');
+	})
+	.catch((err) => {
+		console.log(`Database connection failed!`);
+		console.log(`Details : ${err}`);
+	});
 
 //ROUTES
 
-app.use("/user", userRoute);
-app.use("/board", boardRoute);
+app.use('/user', userRoute);
+app.use('/board', boardRoute);
+app.use('/list', listRoute);
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is online! Port: ${process.env.PORT}`);
+	console.log(`Server is online! Port: ${process.env.PORT}`);
 });
