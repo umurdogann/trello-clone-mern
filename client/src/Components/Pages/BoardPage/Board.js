26c6d76dd47bd888f6ef14d6953097f18adbcd47
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-17 01:44:33 +0300

    add functionality to board page

--- client/src/Components/Pages/BoardPage/Board.js
+++ client/src/Components/Pages/BoardPage/Board.js
@@ -1,21 +1,38 @@
 import Navbar from "../../Navbar";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TopBar from "./BoardComponents/TopBar/TopBar";
 import * as style from "./Styled";
 import AddList from "./BoardComponents/AddList/AddList";
 import List from "./BoardComponents/List/List";
+import { useDispatch, useSelector } from "react-redux";
+import { getBoard } from "../../../Services/boardsService";
+import { getLists } from "../../../Services/boardService";
+import LoadingScreen from "../../LoadingScreen";
+
+
 const Board = (props) => {
   /* props.match.params.id */
-  const [boardName, setBoardName] = useState("Hello World!");  
+  const dispatch = useDispatch();
+  const {backgroundImageLink, loading} = useSelector(state=> state.board);
+  const {allLists, loadingListService } = useSelector(state=> state.list);
+  const boardId = props.match.params.id;
+
+  useEffect(()=>{  
+  getBoard(props.match.params.id,dispatch);
+  getLists(boardId,dispatch);
+  },[props.match.params.id, dispatch, boardId])
+
   return (
     <>
-    
+      {(loading || loadingListService ) && <LoadingScreen/>}
       <Navbar />
-      <style.Container>
-        <TopBar boardName={boardName} callback={setBoardName}/>
+      <style.Container backgroundImg={backgroundImageLink.split('?')[0]}>
+        <TopBar/>
         <style.ListContainer>
-          <List/>
-            <AddList/>
+          {!loading  && allLists.map(list=>{
+            return <List key={list._id} info={list}/>
+          })}
+            <AddList boardId={boardId}/>
         </style.ListContainer>
       </style.Container>
   
