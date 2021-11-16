6d5af7b1bcac9a2488506b9d940c7ad5ff2b1c52
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-17 01:34:56 +0300

    Add getbyid route to boardRoute

--- server/Routes/boardRoute.js
+++ server/Routes/boardRoute.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const boardController = require("../Controllers/boardController");
+const express = require('express');
+const boardController = require('../Controllers/boardController');
 const route = express.Router();
 
-route.post("/create", boardController.create);
-route.get("/",boardController.getAll)
+route.post('/create', boardController.create);
+route.get('/:id', boardController.getById);
+route.get('/', boardController.getAll);
 
 module.exports = route;
