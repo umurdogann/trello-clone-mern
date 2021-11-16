f8839e0d8cf6ff8d3d8d68bc0f81c5a4353e1d10
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-17 01:38:02 +0300

    Update redux store to add board and list reducers

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -2,12 +2,16 @@ import {configureStore} from "@reduxjs/toolkit"
 import userReducer from "./Slices/userSlice";
 import alertReducer from "./Slices/alertSlice";
 import boardsReducer from "./Slices/boardsSlice";
+import boardReducer from "./Slices/boardSlice";
+import listReducer from "./Slices/listSlice";
 
 const Store = configureStore({
     reducer : {
         user: userReducer,
         alert: alertReducer,
         boards: boardsReducer,
+        board: boardReducer,
+        list: listReducer,
     },
 }) 
 export default Store;
