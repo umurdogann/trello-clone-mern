8e6a79b5040dc459b4e341b16110a291e0635868
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-17 01:44:16 +0300

    Add functionality to add list component

--- client/src/Components/Pages/BoardPage/BoardComponents/AddList/AddList.js
+++ client/src/Components/Pages/BoardPage/BoardComponents/AddList/AddList.js
@@ -3,16 +3,24 @@ import * as style from './styled';
 import AddIcon from '@mui/icons-material/Add';
 import BottomButtonGroup from '../BottomButtonGroup/BottomButtonGroup';
 import { TextSpan } from '../../CommonStyled';
+import { useDispatch } from 'react-redux';
+import { createList } from '../../../../../Services/boardService';
 
-const AddList = (prop) => {
+const AddList = (props) => {
+	const dispatch = useDispatch();
 	const [addList, setAddList] = useState(false);
+	const [title,setTitle] = useState("");
+	
 
 	const handleCloseClick = () => {
 		setAddList(false);
+		setTitle("");
 	};
 
 	const handleAddClick = () => {
 		setAddList(false);
+		createList(title,props.boardId,dispatch);
+		setTitle("");
 	};
 
 	return (
@@ -24,7 +32,7 @@ const AddList = (prop) => {
 				</style.AddAnotherListButton>
 				<style.AddListContainer show={addList}>
 					<style.AddListWrapper>
-						<style.ListTitleInput placeholder='Enter list title' />
+						<style.ListTitleInput placeholder='Enter list title' value={title} onChange={(e)=>setTitle(e.target.value)} />
 						<BottomButtonGroup
 							title='Add list'
 							clickCallback={handleAddClick}
