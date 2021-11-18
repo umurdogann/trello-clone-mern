f5cdca1e7390223fd5366a7b3d861f348274f0f8
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-19 02:50:21 +0300

    Format document and add updateListDragDrop action

--- client/src/Redux/Slices/listSlice.js
+++ client/src/Redux/Slices/listSlice.js
@@ -1,40 +1,51 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    allLists:[],
-    loadingListService:true,
-}
+	allLists: [],
+	loadingListService: true,
+};
 
 const listSlice = createSlice({
-    name:"list",
-    initialState,
-    reducers: {
-        setLoading: (state,action)=>{
-            state.loadingListService = action.payload;
-        },
-        successCreatingList: (state,action)=>{
-            state.allLists.push(action.payload);
-        },
-        successFetchingLists: (state,action)=>{
-            state.allLists = action.payload;
-        },
-        successDeletingList: (state, action) => {            
-            state.allLists = state.allLists.filter(list => list._id !== action.payload);
-        },
-        successCreatingCard: (state,action) =>{
-            state.allLists = state.allLists.map(list=>{
-                if(list._id === action.payload.listId){
-                    return action.payload.updatedList;
-                }
-                return list;
-            })
-        },
-        updateCardDragDrop: (state, action) =>{
-            state.allLists = action.payload;
-        }
-    }
+	name: 'list',
+	initialState,
+	reducers: {
+		setLoading: (state, action) => {
+			state.loadingListService = action.payload;
+		},
+		successCreatingList: (state, action) => {
+			state.allLists.push(action.payload);
+		},
+		successFetchingLists: (state, action) => {
+			state.allLists = action.payload;
+		},
+		successDeletingList: (state, action) => {
+			state.allLists = state.allLists.filter((list) => list._id !== action.payload);
+		},
+		successCreatingCard: (state, action) => {
+			state.allLists = state.allLists.map((list) => {
+				if (list._id === action.payload.listId) {
+					return action.payload.updatedList;
+				}
+				return list;
+			});
+		},
+		updateCardDragDrop: (state, action) => {
+			state.allLists = action.payload;
+		},
+		updateListDragDrop: (state, action) => {
+			state.allLists = action.payload;
+		},
+	},
 });
 
-export const {setLoading, successCreatingList, successFetchingLists, successDeletingList, successCreatingCard, updateCardDragDrop} = listSlice.actions;
+export const {
+	setLoading,
+	successCreatingList,
+	successFetchingLists,
+	successDeletingList,
+	successCreatingCard,
+	updateCardDragDrop,
+	updateListDragDrop,
+} = listSlice.actions;
 
-export default listSlice.reducer; 
+export default listSlice.reducer;
