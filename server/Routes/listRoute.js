6cf9ab1b1ecb8489a018e1dc79f5b177c4750463
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-19 01:21:46 +0300

    Add change card order route to listRoute

--- server/Routes/listRoute.js
+++ server/Routes/listRoute.js
@@ -5,5 +5,6 @@ const listController = require('../Controllers/listController');
 router.post('/create', listController.create);
 router.get('/:id', listController.getAll);
 router.delete('/:boardId/:listId', listController.deleteById);
+router.post('/change-card-order', listController.updateCardOrder);
 
 module.exports = router;
