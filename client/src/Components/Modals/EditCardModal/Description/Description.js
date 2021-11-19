f9ba420805f839b27f7b81f473672254e4273b81
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-19 04:37:55 +0300

    Create description component

---
+++ client/src/Components/Modals/EditCardModal/Description/Description.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './styled';
+
+const Description = () => {
+	return <div></div>;
+};
+
+export default Description;
---
+++ client/src/Components/Modals/EditCardModal/Description/styled.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+import { CommonButton } from '../CommonStyles';
+
+export const Container = styled.div``;
