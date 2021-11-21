7845a546f03936b820a741aa532da162612d588e
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-21 04:03:40 +0300

    Add functionality to reusable button

--- client/src/Components/Modals/EditCardModal/ReUsableComponents/Button.js
+++ client/src/Components/Modals/EditCardModal/ReUsableComponents/Button.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Button = () => {
-	return <div></div>;
+export const ButtonStyled = styled.button`
+	display: inline-flex;
+	box-sizing: border-box;
+	align-items: center;
+	justify-content: center;
+	background-color: rgba(0, 0, 0, 0.06);
+	font-size: 0.875rem;
+	border: none;
+	border-radius: 3px;
+	cursor: pointer;
+	color: #172b4d;
+	padding: 0.375rem 0.75rem;
+	&:hover {
+		background-color: rgba(0, 0, 0, 0.1);
+	}
+	&:active {
+		background-color: #e4f0f6;
+		color: #0079bf;
+	}
+`;
+
+const Button = (props) => {
+	const { title, icon, clickCallback } = props;
+	return <ButtonStyled onClick={clickCallback}>{title}</ButtonStyled>;
 };
 
 export default Button;
