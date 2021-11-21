834d1d13d0f971a853341b6c9059a7de19641b0e
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-21 15:06:27 +0300

    Design the actions component

--- client/src/Components/Modals/EditCardModal/Actions/Actions.js
+++ client/src/Components/Modals/EditCardModal/Actions/Actions.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { Container } from './styled';
-
+import { Container, Title } from './styled';
+import Button from '../ReUsableComponents/IconButton';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import CopyIcon from '@mui/icons-material/ContentCopy';
+import WatchIcon from '@mui/icons-material/RemoveRedEyeOutlined';
+import DeleteIcon from '@mui/icons-material/DeleteOutlineOutlined';
 const Actions = () => {
-	return <div></div>;
+	return (
+		<Container>
+			<Title>Actions</Title>
+			<Button title='Move' icon={<ArrowForwardIcon fontSize='1rem' />}></Button>
+			<Button title='Copy' icon={<CopyIcon fontSize='small' />}></Button>
+			<Button title='Watch' icon={<WatchIcon fontSize='small' />}></Button>
+			<Button title='Delete' icon={<DeleteIcon fontSize='small' />}></Button>
+		</Container>
+	);
 };
 
 export default Actions;
