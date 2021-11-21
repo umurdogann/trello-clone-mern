e77f3dfdb273a11a798a71e3a0ff776d1eeafdea
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-21 15:10:34 +0300

    Reformat and update the editcardmodal style page

--- client/src/Components/Modals/EditCardModal/styled.js
+++ client/src/Components/Modals/EditCardModal/styled.js
@@ -1,81 +1,79 @@
 import styled from 'styled-components';
 import { CommonButton } from './CommonStyles';
+import { sm, xs } from '../../../BreakPoints';
 
 export const Container = styled.div`
-box-sizing: border-box;
-background-color: #f4f5f7;
-border-radius: 2px;
-width: 48rem;
-height: 60rem;
-margin: 3rem auto 5rem auto;
-padding: 0.5rem 0rem 0rem 0.25rem;
+	outline: none;
+	box-sizing: border-box;
+	background-color: #f4f5f7;
+	border-radius: 2px;
+	width: 48rem;
+	height: fit-content;
+	margin: 3rem auto 5rem auto;
+	padding: 0.5rem 0rem 1rem 0.25rem;
+	${sm({
+		width: '90%',
+	})}
+	${xs({
+		width: '98%',
+	})}
 `;
 
 export const CoverContainer = styled.div``;
 
 export const TitleContainer = styled.div`
-width:100%;
-margin-bottom: 2rem;
+	width: 100%;
+	margin-bottom: 2rem;
 `;
 
 export const Wrapper = styled.div`
-height:97.5vh;
-width: 100%;
-display: flex;
-flex-direction: row;
-
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	${xs({
+		flexDirection: 'column',
+		paddingRight: '1rem',
+		gap: '1rem',
+	})}
 `;
 
 export const MainContainer = styled.div`
-width: 34.5rem;
-height: 95vh;
-gap: 1.5rem;
-display: flex;
-flex-direction: column;
+	flex: 3;
+	min-height: 50vh;
+	width: 100%;
+	overflow-x: hidden;
+	gap: 1.5rem;
+	display: flex;
+	flex-direction: column;
 `;
 
 export const FeaturesContainer = styled.div`
-width: 100%;
-height:9rem;
+	width: 100%;
 `;
 
 export const DescriptionContainer = styled.div`
-width: 100%;
+	width: 100%;
 `;
 
 export const AttachmentContainer = styled.div`
-border: 1px solid pink;
-width: 100%;
-height:9.75rem;
+	width: 100%;
 `;
 
 export const ChecklistContainer = styled.div`
-border: 1px solid pink;
-width: 100%;
-height:9.75rem;
+	width: 100%;
 `;
 
 export const ActivityContainer = styled.div`
-width: 100%;
+	width: 100%;
 `;
 
 export const RightContainer = styled.div`
-border: 2px dashed green;
-width: 20.5rem;
-height: 95vh;
-display: flex;
-gap: 1.5rem;
-flex-direction: column;
+	display: flex;
+	gap: 1.5rem;
+	flex-direction: column;
+	flex: 1;
 `;
 
-export const AddToCardContainer = styled.div`
-border: 1px dashed red;
-width:10,5rem;
-height:22.75rem;
-`;
+export const AddToCardContainer = styled.div``;
 
-export const ActionsContainer = styled.div`
-border: 1px dashed red;
-width:10,5rem;
-height:16.5rem;
-`;
+export const ActionsContainer = styled.div``;
