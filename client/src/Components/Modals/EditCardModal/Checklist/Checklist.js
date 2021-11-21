0aa08a77cf79e14c46a5ff26d46544d50f019011
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-21 04:06:54 +0300

    Design the checklist component

--- client/src/Components/Modals/EditCardModal/Checklist/Checklist.js
+++ client/src/Components/Modals/EditCardModal/Checklist/Checklist.js
@@ -1,8 +1,72 @@
-import React from 'react';
-import { Container } from './styled';
-
+import React, { useState } from 'react';
+import {
+	Container,
+	Row,
+	LeftColumn,
+	RightColumn,
+	Title,
+	Percentage,
+	CheckText,
+	TextAreaContainer,
+	TextArea,
+} from './styled';
+import CheckIcon from '@mui/icons-material/LibraryAddCheckOutlined';
+import BottomButtonGroup from '../../../Pages/BoardPage/BoardComponents/BottomButtonGroup/BottomButtonGroup.js';
+import Checkbox from '../ReUsableComponents/Checkbox';
+import Button from '../ReUsableComponents/Button';
+import Progressbar from '../ReUsableComponents/Progressbar';
 const Checklist = () => {
-	return <div></div>;
+	const [checked, setChecked] = useState(false);
+	const [showAddItem, setShowAddItem] = useState(false);
+	const [newItem, setNewItem] = useState('');
+	return (
+		<Container>
+			<Row>
+				<LeftColumn>
+					<CheckIcon fontSize='small' />
+				</LeftColumn>
+				<RightColumn>
+					<Title>CheckList</Title>
+					<Button title='Delete' />
+				</RightColumn>
+			</Row>
+			<Row>
+				<LeftColumn>
+					<Percentage>0%</Percentage>
+				</LeftColumn>
+				<RightColumn>
+					<Progressbar value={50} />
+				</RightColumn>
+			</Row>
+
+			<Row>
+				<LeftColumn>
+					<Checkbox checked={checked} clickCallback={setChecked} />
+				</LeftColumn>
+				<RightColumn>
+					<CheckText isChecked={checked}>This is a sample check text.</CheckText>
+				</RightColumn>
+			</Row>
+
+			<Row>
+				<LeftColumn></LeftColumn>
+				<RightColumn>
+					{showAddItem ? (
+						<TextAreaContainer>
+							<TextArea
+								value={newItem}
+								onChange={(e) => setNewItem(e.target.value)}
+								placeholder='Add an item'
+							/>
+							<BottomButtonGroup title='Add' closeCallback={() => setShowAddItem(false)} />
+						</TextAreaContainer>
+					) : (
+						<Button clickCallback={() => setShowAddItem(true)} title='Add an item' />
+					)}
+				</RightColumn>
+			</Row>
+		</Container>
+	);
 };
 
 export default Checklist;
--- client/src/Components/Modals/EditCardModal/Checklist/styled.js
+++ client/src/Components/Modals/EditCardModal/Checklist/styled.js
@@ -1,4 +1,91 @@
 import styled from 'styled-components';
-import { CommonButton } from '../CommonStyles';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	padding-left: 1rem;
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+	width: 100%;
+	height: fit-content;
+`;
+
+export const Row = styled.div`
+	display: flex;
+	width: 100%;
+	flex-direction: row;
+	align-items: center;
+	gap: 1rem;
+`;
+
+export const LeftColumn = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+	width: 2rem;
+`;
+
+export const RightColumn = styled.div`
+	display: flex;
+	width: 100%;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+`;
+
+export const Title = styled.div`
+	margin: 0;
+	padding: 0.2rem 0rem 0rem 0rem;
+	display: inline;
+	color: black;
+	font-size: 1rem;
+	font-weight: 800;
+`;
+
+export const Percentage = styled.div`
+	color: #5e6c84;
+	font-size: 0.75rem;
+	width: 1.5rem;
+	text-align: center;
+`;
+
+export const CheckText = styled.div`
+	text-decoration: ${(props) => (props.isChecked ? 'line-through' : 'none')};
+	color: ${(props) => (props.isChecked ? '#5e6c84' : '#172b4d')};
+	font-size: 0.875rem;
+`;
+
+export const TextAreaContainer = styled.div`
+	display: flex;
+	width: 100%;
+	flex-direction: column;
+	gap: 0.5rem;
+`;
+
+export const TextArea = styled.textarea`
+	box-sizing: content-box;
+	width: 100%;
+	min-height: 2.5rem;
+	background-color: rgba(0, 0, 0, 0.03);
+	border: none;
+	border-radius: 2px;
+	box-shadow: none;
+	font-size: 0.875rem;
+	outline: 2px solid transparent;
+	padding: 0.5rem 0.75rem;
+	resize: none;
+	cursor: pointer;
+	transition: 150 ease-in;
+	&:hover {
+		background-color: rgba(0, 0, 0, 0.06);
+	}
+	&::placeholder {
+		color: black;
+	}
+	&:focus {
+		&::placeholder {
+			color: gray;
+		}
+		background-color: #fff;
+		outline: 2px solid #0079bf;
+	}
+`;
