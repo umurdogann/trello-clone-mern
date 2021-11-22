73fd402fdf8014614378be66a34ac021a496a580
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-22 03:31:59 +0300

    Add getCard and update functions to card service

--- server/Services/cardService.js
+++ server/Services/cardService.js
@@ -1,19 +1,17 @@
 const cardModel = require('../Models/cardModel');
 const listModel = require('../Models/listModel');
 const boardModel = require('../Models/boardModel');
+const helperMethods = require('./helperMethods');
 
 const create = async (title, listId, boardId, user, callback) => {
 	try {
 		// Get list and board
-		
 		const list = await listModel.findById(listId);
 		const board = await boardModel.findById(boardId);
 
 		// Validate the ownership
-		const validate = user.boards.filter((board) => board.toString() === boardId);
-		const validate2 = board.lists.filter((list) => list.toString() === listId);
-		if (!(validate && validate2))
-			return callback({ errMessage: 'You dont have permission to add card to this list or board' });
+		const validate = await helperMethods.validateCardOwners(null, list, board, user, true);
+		if (!validate) return callback({ errMessage: 'You dont have permission to add card to this list or board' });
 
 		// Create new card
 		const card = await cardModel({ title: title });
@@ -21,7 +19,6 @@ const create = async (title, listId, boardId, user, callback) => {
 		await card.save();
 
 		// Add id of the new card to owner list
-
 		list.cards.push(card._id);
 		await list.save();
 
@@ -30,10 +27,52 @@ const create = async (title, listId, boardId, user, callback) => {
 			action: `${user.name + ' ' + user.surname} added ${card.title} to this board`,
 		});
 		await board.save();
-		
+
 		// Set data transfer object
-		const result = await listModel.findById(listId).populate({ path: 'cards', select: 'title' }).exec();		
+		const result = await listModel.findById(listId).populate({ path: 'cards', select: 'title' }).exec();
 		return callback(false, result);
+	} catch (error) {
+		return callback({ errMessage: 'Something went wrong', details: error.message });
+	}
+};
+
+const getCard = async (cardId, listId, boardId, user, callback) => {
+	try {
+		// Get models
+		const card = await cardModel.findById(cardId);
+		const list = await listModel.findById(listId);
+		const board = await boardModel.findById(boardId);
+
+		// Validate owner
+		const validate = await helperMethods.validateCardOwners(card, list, board, user, false);
+		if (!validate) {
+			errMessage: 'You dont have permission to update this card';
+		}
+
+		return callback(false, card);
+	} catch (error) {
+		return callback({ errMessage: 'Something went wrong', details: error.message });
+	}
+};
+
+const update = async (cardId, listId, boardId, user, updatedObj, callback) => {
+	try {
+		// Get models
+		const card = await cardModel.findById(cardId);
+		const list = await listModel.findById(listId);
+		const board = await boardModel.findById(boardId);
+
+		// Validate owner
+		const validate = await helperMethods.validateCardOwners(card, list, board, user, false);
+		if (!validate) {
+			errMessage: 'You dont have permission to update this card';
+		}
+
+		//Update card
+		await card.update(updatedObj);
+		await card.save();
+
+		return callback(false, { message: 'Success!' });
 	} catch (error) {
 		return callback({ errMessage: 'Something went wrong', details: error.message });
 	}
@@ -41,4 +80,6 @@ const create = async (title, listId, boardId, user, callback) => {
 
 module.exports = {
 	create,
+	update,
+	getCard,
 };
