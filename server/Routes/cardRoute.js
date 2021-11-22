706f7c06cdb78d64dcc527dcf9936c7192a1f728
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-22 03:34:34 +0300

    Add get and update routes for cards

--- server/Routes/cardRoute.js
+++ server/Routes/cardRoute.js
@@ -3,5 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/create', cardController.create);
+router.get('/:boardId/:listId/:cardId', cardController.getCard);
+router.put('/:boardId/:listId/:cardId', cardController.update);
 
 module.exports = router;
