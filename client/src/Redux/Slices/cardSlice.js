dd89a8d6b8ca9e859cba1e49885896a76a3ed736
Cuma Umur Doğan <umurdogann@gmail.com>
2021-11-22 03:47:45 +0300

    Create redux slice for card

---
+++ client/src/Redux/Slices/cardSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+	cardId: '',
+	title: '',
+	labels: [],
+	members: [],
+	watchers: [],
+	activities: [],
+	owner: '',
+	loading: false,
+};
+
+const cardsSlice = createSlice({
+	name: 'card',
+	initialState,
+	reducers: {		
+		setLoading: (state, action) => {
+			state.loading = action.payload;
+		},
+	},
+});
+
+export const { reset, setLoading } = cardsSlice.actions;
+export default cardsSlice.reducer;
